57ca8275f2dfa6e1e18b70fec77104af8d4c4e76
qbentil <bentilshadrack72@gmail.com>
2023-01-03 11:19:16 +0000

    Quick Test

--- server.ts
+++ server.ts
@@ -1,11 +1,22 @@
-import express from 'express';
+import express, { Response, Request } from 'express';
 // init App
 const app = express();
 app.use(express.json());
 
-app.get('/', (req, res) => {
+app.get('/', (req:Request, res:Response) => {
   res.send('Hello CrownCast!');
 });
+
+app.get('/api', (req:Request, res:Response) => {
+    res.status(200).json({
+        message: 'Hello CrownCast!',
+        data: {
+            name: 'CrownCast',
+            age: 1,
+            author: 'CrownCast'
+        }
+    })
+})
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
