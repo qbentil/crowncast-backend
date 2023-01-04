dfb3722839900106c5645e7555aced89ae00bba7
qbentil <bentilshadrack72@gmail.com>
2023-01-04 10:44:39 +0000

    Create Organizer model

---
+++ models/organizer.model.ts
@@ -0,0 +1,47 @@
+import { Schema } from "mongoose";
+
+const CompanySchema = new Schema({
+    name: {
+        type: String,
+        required: [true, "Name is required"],
+    },
+    address: {
+        type: String,
+        required: [true, "Address is required"],
+    },
+    phone: {
+        type: String,
+        required: [true, "Phone is required"],
+    },
+})
+
+const OrganizerSchema = new Schema({
+    name: {
+        type: String,
+        required: [true, "Name is required"],
+    },
+    email: {
+        type: String,
+        required: [true, "Email is required"],
+    },
+    phone: {
+        type: String,
+        required: [true, "Phone is required"],
+    },
+    company: {
+        type: CompanySchema,
+        required: [true, "Company is required"],
+    },
+    password: {
+        type: String,
+        required: [true, "Password is required"],
+    },
+    address: {
+        type: String,
+    },
+    is_deleted: {
+        type: Boolean,
+        default: false
+    }
+}, { timestamps: true });
+
