0b229f6a7db137b94911172737b38a1e57b4ea07
qbentil <bentilshadrack72@gmail.com>
2023-01-04 10:43:48 +0000

    Update Server connection

--- server.ts
+++ server.ts
@@ -1,23 +1,15 @@
 import express, { Response, Request } from 'express';
+import dotenv from 'dotenv';
+import DBCONNECT from './config/dbconnection';
+dotenv.config();
+const PORT = process.env.PORT || 3000;
 // init App
 const app = express();
 app.use(express.json());
 
-app.get('/', (req:Request, res:Response) => {
-  res.send('Hello CrownCast!');
-});
 
-app.get('/api', (req:Request, res:Response) => {
-    res.status(200).json({
-        message: 'Hello CrownCast!',
-        data: {
-            name: 'CrownCast',
-            age: 1,
-            author: 'CrownCast'
-        }
-    })
-})
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+app.listen(PORT, async() => {
+    await DBCONNECT();
+    console.log(`Server running on port ${PORT}🚀`);
 })
