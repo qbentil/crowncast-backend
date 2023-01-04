1a0474a109fbd701272f79e52898693be44a8e40
qbentil <bentilshadrack72@gmail.com>
2023-01-04 10:44:29 +0000

    Create event model

---
+++ models/event.model.ts
@@ -0,0 +1,39 @@
+import { Schema } from 'mongoose';
+const EventSchema = new Schema({
+    name: {
+        type: String,
+        required: [true, 'Name is required'],
+    },
+    description: {
+        type: String,
+        required: [true, 'Description is required'],
+    },
+    banner: {
+        type:String,
+        required:[true, "Please enter event image"],
+    },
+    vote_price: {
+        type: Number,
+        required: [true, 'Vote price is required'],
+    },
+    votes: {
+        type: Number,
+        default: 0,
+    },
+    opening_date: {
+        type: Date,
+        required: [true, 'Opening date is required'],
+    },
+    closing_date: {
+        type: Date,
+        required: [true, 'Closing date is required'],
+    },
+    organizer: {
+        type: Schema.Types.ObjectId,
+        ref: 'Organizer',
+    },
+    is_deleted: {
+        type: Boolean,
+        default: false,
+    },
+}, { timestamps: true });
