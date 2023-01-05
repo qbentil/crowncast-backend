730b3297ee4ffade172502dc1990f0b005929032
qbentil <bentilshadrack72@gmail.com>
2023-01-05 17:33:46 +0000

    Create models export file

---
+++ models/index.ts
@@ -0,0 +1,5 @@
+export {default as Event } from './event.model';
+export {default as User } from './user.model';
+export {default as Organizer } from './organizer.model';
+export {default as Contestant } from './contestant.model';
+export {default as Category } from './category.model';
