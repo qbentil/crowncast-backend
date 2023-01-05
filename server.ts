1ec08ff243e97d330b751d5669efe60e07655d8c
qbentil <bentilshadrack72@gmail.com>
2023-01-05 17:34:07 +0000

    Update Server

--- server.ts
+++ server.ts
@@ -1,12 +1,18 @@
 import express, { Response, Request } from 'express';
+import ErrorHandler from './middleware/errorHandler';
 import dotenv from 'dotenv';
 import DBCONNECT from './config/dbconnection';
+import { EventRoute } from './routes';
 dotenv.config();
 const PORT = process.env.PORT || 3000;
 // init App
 const app = express();
 app.use(express.json());
 
+app.use("/event", EventRoute)
+
+// ERROR HANDLER MIDDLEWARE
+app.use(ErrorHandler);
 
 
 app.listen(PORT, async() => {
