ccab457bd4a368fb1274ed3d9250bdb86cb3437b
qbentil <bentilshadrack72@gmail.com>
2023-01-06 10:48:05 +0000

    ✨Setup Organizer route

--- server.ts
+++ server.ts
@@ -2,7 +2,7 @@ import express, { Response, Request } from 'express';
 import ErrorHandler from './middleware/errorHandler';
 import dotenv from 'dotenv';
 import DBCONNECT from './config/dbconnection';
-import { EventRoute } from './routes';
+import { EventRoute, OrganizerRoute } from './routes';
 dotenv.config();
 const PORT = process.env.PORT || 3000;
 // init App
@@ -10,6 +10,7 @@ const app = express();
 app.use(express.json());
 
 app.use("/event", EventRoute)
+app.use("/organizer", OrganizerRoute)
 
 // ERROR HANDLER MIDDLEWARE
 app.use(ErrorHandler);
