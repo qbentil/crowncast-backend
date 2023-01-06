839afe508e69cc40f66d857df767ab69f7ae4eab
qbentil <bentilshadrack72@gmail.com>
2023-01-06 02:30:50 +0000

    Update contestant model

--- models/contestant.model.ts
+++ models/contestant.model.ts
@@ -9,6 +9,10 @@ const ContestantSchema = new Schema({
         type: String,
         required: [true, "Image is required"],
     },
+    code: {
+        type: String,
+        required: [true, "Code is required"],
+    },
     votes: {
         type: Number,
         default: 0,
