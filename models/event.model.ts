71441c7105418bb89d9c124b16d312cd1b758155
qbentil <bentilshadrack72@gmail.com>
2023-01-06 02:31:02 +0000

    Update event model

--- models/event.model.ts
+++ models/event.model.ts
@@ -32,11 +32,6 @@ const EventSchema = new Schema({
         type: Schema.Types.ObjectId,
         ref: 'Organizer',
     },
-    categories: {
-        type: [Schema.Types.ObjectId],
-        default: [],
-        ref: 'Category'
-    },
     is_deleted: {
         type: Boolean,
         default: false,
