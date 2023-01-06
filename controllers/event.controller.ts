fbaad8edc7192fa4139e996d0df4d109919b40cb
qbentil <bentilshadrack72@gmail.com>
2023-01-06 02:29:05 +0000

    Update event controller, add event

--- controllers/event.controller.ts
+++ controllers/event.controller.ts
@@ -1,6 +1,30 @@
 import { NextFunction, Request, Response } from "express";
 import Event from "../models/event.model";
 
+// CREATE EVENT
+const addEvent = async (req: Request, res: Response, next: NextFunction) => {
+    const { name, description, banner, vote_price, opening_date, closing_date, organizer } = req.body;
+    try {
+        const event = await Event.create({
+            name,
+            description,
+            banner,
+            vote_price,
+            opening_date,
+            closing_date,
+            organizer,
+        });
+        res.status(201).json({
+            success: true,
+            data: event,
+            message: "Event created successfully",
+        });
+    } catch (err) {
+        next(err);
+    }
+}
+
+// FETCH ALL EVENTS
 const getEvents = async (req: Request, res: Response, next: NextFunction) => {
     try {
         const events = await Event.find({ is_deleted: false });
@@ -13,5 +37,6 @@ const getEvents = async (req: Request, res: Response, next: NextFunction) => {
         next(err);
     }
 }
+
 
 export { getEvents };
