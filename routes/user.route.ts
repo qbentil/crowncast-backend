c5e309d3d174f89e638d4a781fdbbd57582a9575
qbentil <bentilshadrack72@gmail.com>
2023-01-07 18:57:34 +0000

    ✨: Add change password & reset password to route

--- routes/user.route.ts
+++ routes/user.route.ts
@@ -1,6 +1,6 @@
 import { VerifyAccessToken, VerifyAdmin } from './../middleware/verifications';
 import { Router } from 'express';
-import { addUser, getAllUsers, login } from '../controllers/user.controller';
+import { addUser, changePassword, getAllUsers, login, resetPassword } from '../controllers/user.controller';
 
 const router = Router();
 
@@ -12,5 +12,11 @@ router.post('/', VerifyAccessToken, VerifyAdmin, addUser);
 
 // GET ALL USERS
 router.get('/', VerifyAccessToken, VerifyAdmin, getAllUsers);
+
+//  CHANGE PASSWORD
+router.put("/password", VerifyAccessToken, changePassword);
+
+// RESET PASSWORD 
+router.put("/reset-password/:id", VerifyAccessToken, VerifyAdmin, resetPassword);
 
 export default router;
