3f1d942c2086173fc04c82ddecafe5421030a617
qbentil <bentilshadrack72@gmail.com>
2023-01-07 18:57:14 +0000

    ✨: Create change password & reset password mailer functions

--- util/index.ts
+++ util/index.ts
@@ -39,6 +39,14 @@ export const ComparePassword = async (password: string, hashedPassword: string) 
     return await bcrypt.compare(password, hashedPassword);
 }
 
+
+export const HashPassword = async (password: string) => {
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    return hashedPassword;
+}
+
 export const GenerateToken = (user: any, role: string) => {
     //   Create access token
     const accessToken = jwt.sign(
